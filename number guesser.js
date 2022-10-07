ab18164da217617ee823823723afba51e3c060c4
renuu <108678640+renuutomer@users.noreply.github.com>
2022-10-07 12:57:03 +0100

    Create number guesser.js

---
+++ number guesser.js
@@ -0,0 +1,31 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+const generateTarget = ()=> {
+return Math.floor(Math.random() * 10);
+};
+const compareGuesses = (humanGuess, computerGuess, targetGuess)=> {
+const humanDifference = Math.abs(targetGuess - humanGuess );
+const computerDifference = Math.abs(targetGuess - computerGuess )
+if (humanDifference < computerDifference) {
+return 'Human won!'
+} else if (computerDifference < humanDifference) {
+return 'Computer won!'
+} else {
+return 'Human won!'
+};
+};
+const updateScore = winner => {
+if(winner === 'human'){
+humanScore++;
+}
+else if (winner === 'computer'){
+computerScore++;
+}
+};
+const advanceRound = ()=>{
+currentRoundNumber = currentRoundNumber + 1;
+};
